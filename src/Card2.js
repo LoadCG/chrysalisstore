b02eca58eeed40de7dccb71f199474fd0db8650b
Cauan Gabriel <cauangabrielresende@gmail.com>
2024-03-23 19:34:15 -0300

    cards

--- src/Card2.js
+++ src/Card2.js
@@ -6,18 +6,26 @@ const Card2 = () => {
         <div className="container p-2">
             <div className="row">
                 <div className="col-lg-4 col-md-6 mb-4">
-                    <div className="card text-center" style={{ width: '18rem', backgroundColor: '#fff' }}>
+                    <div className="card text-center" style={{ width: '18rem', backgroundColor: '#f8f9fa' }}>
                         <img src={image} className="card-img-top" alt="Imagem do Produto" />
                         <div className="card-body">
-                            <h5 className="card-title" style={{ color: '#332F2B' }}>TITULO DO PRODUTO</h5>
-                            <p className="card-text text-dark">R$ 00,00</p>
-                            <a href="#" className="btn btn-outline-light text-light center" style={{ backgroundColor: '#ffbc49' }}>CLIQUE AQUI!</a>
+                            <h5 className="card-title" style={{ color: '332F2B' }}>Best Seller Tech T-Shirt</h5>
+                            <p className="card-text text-dark" style={{ color: '332F2B' }}>
+                                Evolução tecnológica da camiseta básica,
+                                <br />
+                                anti odor, não precisa ser passada
+                            </p>
+                            <p className="card-text text-dark">
+                                <b>R$ 159</b>
+                            </p>
+                            <a href="#" className="btn btn-outline-dark" style={{ backgroundColor: '#ffbc49' }}>
+                                CLIQUE AQUI!
+                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
     )
 }
 
--- src/Category.js
+++ src/Category.js
@@ -5,9 +5,15 @@ import Card2 from './Card2.js';
 const Category = () => {
     return (
         <div className='d-grid ' style={{backgroundColor:'#332f2b', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '20px', padding: '20px', zIndex:'12'}}>
-            <Card2 />
-            <Card2 />
-            <Card2 />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
             <Card />
             <Card />
             <Card />
